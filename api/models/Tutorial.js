36bd4310785234cfb19a4b6e2408269ed8aeecce
Umar <umar@yagnitech.com>
2020-05-30 06:19:55 +0530

    Add tutorials model and attributes

---
+++ api/models/Tutorial.js
@@ -0,0 +1,52 @@
+/**
+ * Tutorial.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    title: {
+      type: 'string',
+      required: true
+    },
+
+    summary: {
+      type: 'string'
+    },
+
+    author: {
+      type: 'string',
+      required: true
+    },
+
+    status: {
+      type: 'string',
+      isIn: ['draft', 'published', 'retracted'],
+      defaultsTo: 'draft'
+    },
+
+    content: {
+      type: 'string'
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
