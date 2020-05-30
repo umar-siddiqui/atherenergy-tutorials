78c4afb34a851e7613a28385fb86b9a0f74a9801
Umar <umar@yagnitech.com>
2020-05-30 06:24:22 +0530

    Add tutorial controller and nodemon for auto reload

---
+++ api/controllers/TutorialController.js
@@ -0,0 +1,30 @@
+/**
+ * TutorialController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `TutorialController.find()`
+   */
+  find: async function (req, res) {
+    return res.json({
+      todo: 'find() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `TutorialController.destroyAll()`
+   */
+  destroyAll: async function (req, res) {
+    return res.json({
+      todo: 'destroyAll() is not implemented yet!'
+    });
+  }
+
+};
+
