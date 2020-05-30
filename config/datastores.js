245ceb7b120df7a69eb94ea792942f1771152ac3
Umar <umar@yagnitech.com>
2020-05-30 07:12:05 +0530

    Config postgres for dev env

--- config/datastores.js
+++ config/datastores.js
@@ -50,6 +50,8 @@ module.exports.datastores = {
     ***************************************************************************/
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-postgresql',
+    url: 'postgresql://atheradmin:atheradmin@localhost:5432/atherenergy_tutorials',
 
   },
 
